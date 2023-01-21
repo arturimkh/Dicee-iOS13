6cbb9387987a9bb7610d09b6bd4ac0e9f5dec250
Artur <imanbaevart123@gmail.com>
2023-01-22 01:22:15 +0300

    final

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var arr = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        diceImageView1.image = arr[Int.random(in: 0...5)]
+        diceImageView2.image = arr[Int.random(in: 0...5)]
+    }
 }
 
